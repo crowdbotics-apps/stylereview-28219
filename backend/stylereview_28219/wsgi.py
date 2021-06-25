66bad52110f1da10372bb0bddd4db723f5feb725
Crowdbotics <team@crowdbotics.com>
2021-06-25 01:29:20 +0000

    Update models

--- backend/stylereview_28219/wsgi.py
+++ backend/stylereview_28219/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stylereview_28219.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stylereview_28219.settings")
 
 application = get_wsgi_application()
